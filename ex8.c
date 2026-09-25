570347e40c40a776a53ce88716f8eb44688b1aaa
anaclaraperessim <anaperessimm@gmail.com>
2026-09-24 21:22:10 -0300

    Improve user prompts in number input program

--- ex8.c
+++ ex8.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int numero;
 
+    printf("Digite um numero: ");
     scanf("%d", &numero);
 
     while(numero != 0) {
-        printf("Você digitou %d\n", numero);
+        printf("Voce digitou %d\n", numero);
 
+        printf("Digite outro numero: ");
         scanf("%d", &numero);
     }
 
     printf("Programa encerrado.\n");
 
     return 0;
-
 }
